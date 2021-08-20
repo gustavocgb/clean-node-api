e4c8fc5f68bb37a53fe1f346857b295ace7043d8
gustavocgb11@gmail.com <gustavocgb11@gmail.com>
2021-08-20 15:25:35 -0300

    refactor: removed code duplicate

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -11,14 +11,17 @@ export class SignUpController implements Controller {
 
   handle (httpRequest: HttpRequest): any {
     try {
+      const { email, password, passwordConfirmation } = httpRequest.body
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
           return badRequest(new MissingParamError(field))
         }
       }
-
-      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      if (password !== passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
+      const isValid = this.emailValidator.isValid(email)
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
