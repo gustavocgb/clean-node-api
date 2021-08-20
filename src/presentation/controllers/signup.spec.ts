c233afd2d1f52209c5b8f7b005c221170e0059b9
gustavocgb11@gmail.com <gustavocgb11@gmail.com>
2021-08-20 15:16:57 -0300

    refractor: change factor error

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -17,14 +17,15 @@ const makeEmailValidador = (): EmailValidator => {
   return new EmailValidatorStub()
 }
 
-const makeEmailValidadorError = (): EmailValidator => {
+// foi substituido pela funcao do jest spyOn().mockImplementationOnce
+/* const makeEmailValidadorError = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
     isValid (email: string): boolean {
       throw new Error()
     }
   }
   return new EmailValidatorStub()
-}
+} */
 
 const makeSut = (): SutTypes => {
   const emailValidatorStub = makeEmailValidador()
@@ -126,8 +127,12 @@ describe('SignUp Controller', () => {
   })
 
   test('Should return 500 if EmailValidator throws', () => {
-    const emailValidatorStub = makeEmailValidadorError()
-    const sut = new SignUpController(emailValidatorStub)
+    // const emailValidatorStub = makeEmailValidadorError()
+    // const sut = new SignUpController(emailValidatorStub)
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
+      throw new Error()
+    })
     const httpRequest = {
       body: {
         name: 'any_name',
